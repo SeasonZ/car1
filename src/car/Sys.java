7eb540ba121febd0285a03c51affa165d1e2605b
SeasonZ <570878181@qq.com>
2016-05-05 15:49:42 +0800

    car1.1

--- src/car/Sys.java
+++ src/car/Sys.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Sys {
 	public void rentMoto() {
+		double[] Money = new double[10];
+		
 		String[] carType = { "宝马", "奔驰", "奥迪" };
 		int[] busType = {10,32};
 		Scanner input = new Scanner(System.in);
@@ -11,21 +13,25 @@ public class Sys {
 		
 		System.out.println("请问租几辆车:");
 		int num = input.nextInt();
-		//租几天
-		System.out.println("day");
+		
+		for (int n = 1; n <=num; n++) {
+			
+		//第n量车租几天
+		System.out.println("第"+n+"辆车租天数：");
 		int day = input.nextInt();
-		System.out.println("1、car or 2、bus");
+		//第n量车种类
+		System.out.println("车种类：1、car or 2、bus");
 		int sel = input.nextInt();
 		if (sel == 1) {
-			System.out.println("type 1、bmw 2、audi 3、benc");
+			System.out.println("车品牌： 1、bmw 2、audi 3、benc");
 			sel = input.nextInt();
 			Moto moto = new Car("鲁 B88888");
 			Car car = (Car)moto;
 			car.setType(carType[sel - 1]);
 			// 计算租金
-			double totalFee = moto.rentFee(day);
+			Money[n] = moto.rentFee(day);
 			// 输出结果
-			System.out.println("**car , * day , totalfee ***" + totalFee);
+//			System.out.println("**car , * day , totalfee ***" + totalFee);
 		} else {
 			System.out.println("seatCount 1、 < 16 2、 > 16");
 			sel = input.nextInt();
@@ -33,10 +39,16 @@ public class Sys {
 			Bus bus = (Bus)moto;
 			bus.setSeatCount(busType[sel - 1]);
 			// 计算租金
-			double totalFee = moto.rentFee(day);
+	        Money[n] = moto.rentFee(day);
 			// 输出结果
-			System.out.println("**car , * day , totalfee ***" + totalFee);
+//			System.out.println("**car , * day , totalfee ***" + totalFee);
 		}
+		}
+		double total = 0;
+		for (int i = 1; i <=num; i++) {
+			total +=Money[i];
+		}
+		System.out.println("合集车费是："+total);
 	}
 	public static void main(String[] args) {
 		new Sys().rentMoto();
